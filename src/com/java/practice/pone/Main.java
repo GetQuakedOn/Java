8fb00c225b2deec5bdee30b69e8e428a032e9943
GetQuakedOn <syvvvus@gmail.com>
2020-08-16 00:01:41 +0300

    Update

--- src/com/java/practice/pone/Main.java
+++ src/com/java/practice/pone/Main.java
@@ -1,16 +1,32 @@
 package com.java.practice.pone;
 
-import com.java.practice.pthree.ForLoop;
+import com.java.practice.pfour.TV;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        ForLoop fl = new ForLoop();
-        fl.Loop();
+        TV tv1 = new TV(true, 1, 70);
+        TV tv2 = new TV(false, 2, 50);
 
+        System.out.println("TV1 param");
+        tv1.GetParam();
+
+        Spaces(1);
+
+        System.out.println("TV2 param");
+        tv2.GetParam();
+
+        Spaces(1);
+
+        tv1.numOfChannel = tv2.numOfChannel;
+        tv1.GetParam();
     }
 
-    public static int Sum(int a, int b) {
-        return a + b;
+    public static void Spaces(int value) {
+        for (int i = 0; i < value; i++) {
+            System.out.println();
+        }
     }
+
 }
