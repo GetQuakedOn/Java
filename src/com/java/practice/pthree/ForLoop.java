8d4f9ab092afbe7c43127bd6f167684f00138a86
GetQuakedOn <syvvvus@gmail.com>
2020-08-15 22:12:40 +0300

    Update

--- src/com/java/practice/pthree/ForLoop.java
+++ src/com/java/practice/pthree/ForLoop.java
@@ -7,7 +7,7 @@ public class ForLoop {
 //            System.out.println(i);
 //        }
         int a = 0;
-        while (a < 10) {
+        while (a < 9) {
             System.out.println(a);
             a++;
         }
