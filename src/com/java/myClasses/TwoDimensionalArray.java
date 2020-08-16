0957db3f292828d4669985032a31c7309a4413d6
GetQuakedOn <syvvvus@gmail.com>
2020-08-17 01:20:57 +0300

    New package New class

---
+++ src/com/java/myClasses/TwoDimensionalArray.java
@@ -0,0 +1,33 @@
+package com.java.myClasses;
+
+import java.lang.Math;
+
+public class TwoDimensionalArray {
+
+    private int ROW, COL;
+    private int arr[][];
+
+    public TwoDimensionalArray(final int ROW, final int COL) {
+        this.ROW = ROW;
+        this.COL = COL;
+
+        arr = new int[ROW][COL];
+    }
+
+    public void FillArray(final int value) {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                arr[i][j] = (int)(Math.random() * value);
+            }
+        }
+    }
+
+    public void ShowArray() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/com/java/practice/pone/Main.java
+++ src/com/java/practice/pone/Main.java
@@ -4,55 +4,38 @@ import com.java.practice.pfour.TV;
 import com.java.practice.ptwo.Bus;
 import java.lang.Math;
 import java.util.Scanner;
+import com.java.myClasses.TwoDimensionalArray;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("In");
-        //int x = in.nextInt();
-        if (in.hasNextInt()) {
-            int x = in.nextInt();
-            System.out.println(x);
-        } else {
-            System.out.println("Fck off");
+    TwoDimensionalArray arr = new TwoDimensionalArray(3, 5);
+
+//    arr.FillArray(100);
+//    arr.ShowArray();
+
+    int Mass[][] = new int[3][5];
+
+    for (int i = 0; i < 3; i++) {              // заполнение
+        for (int j = 0; j < 5; j++) {
+            Mass[i][j] = (int)(Math.random() * 10);
         }
+    }
 
-        //
-
-        in.close();
-
-        int y = in.nextInt();
-        System.out.println(y);
-
-        //
-
-        /*int array[] = {1, 2, 3, 4, 5};
-
-        for (int element: array) {
-            System.out.print(element + "\t");
-        }
-
-        Scanner in = new Scanner();
-
-        Spaces(2);
-
-        final int ROW = 3, COL = 5;
-
-        int arr[][] = new int[ROW][COL];
-
-        for (int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++) {
-                arr[i][j] = (int) (Math.random() * 10);
+        for (int i = 0; i < 3; i++) {          // вывод
+            for (int j = 0; j < 5; j++) {
+                System.out.print(Mass[i][j] + "\t");
             }
+            System.out.println();
         }
 
-        for (int i = 0; i < ROW; i++) {
-            for (int j = 0; j < COL; j++) {
-                System.out.print(arr[i][j] + "\t");
+    Spaces(2);
+
+        /*for (int i[]: Mass) {
+            for (int j : i) {
+                System.out.println(i);
             }
-            Spaces(1);
         }*/
 
     }
