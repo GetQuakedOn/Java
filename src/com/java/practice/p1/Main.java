5b193b765fcf38ff5b9aade75504c05bb35dbddd
GetQuakedOn <syvvvus@gmail.com>
2020-09-02 02:14:24 +0300

    New text file and scan

--- src/com/java/practice/p1/Main.java
+++ src/com/java/practice/p1/Main.java
@@ -2,11 +2,23 @@ package com.java.practice.p1;
 
 import com.java.practice.p17.A;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        //
+        File file = new File("text");
+        Scanner scanner = new Scanner(file);
+        String str = scanner.nextLine();
+        System.out.println(str);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+
+        scanner.close();
 
     }
 
