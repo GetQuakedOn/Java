794e2a08be9e63df329b357bff9801b97097fd00
GetQuakedOn <syvvvus@gmail.com>
2020-09-10 18:11:56 +0300

    Added MyLinkedList

--- src/com/java/practice/p1/Main.java
+++ src/com/java/practice/p1/Main.java
@@ -1,10 +1,17 @@
 package com.java.practice.p1;
 
+import com.java.practice.p22.MyLinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        //
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+
+        System.out.println(myLinkedList);
 
     }
 
