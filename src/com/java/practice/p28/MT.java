30f38bb287ed43ab59c00aed62f323f5e62b7f9a
GetQuakedOn <syvvvus@gmail.com>
2020-10-14 16:12:26 +0300

    added volatile var

--- src/com/java/practice/p28/MT.java
+++ src/com/java/practice/p28/MT.java
@@ -1,10 +1,21 @@
 package com.java.practice.p28;
 
 public class MT extends Thread {
+    private volatile boolean running = true;
+
     @Override
     public void run() {
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(i);
+        while (running) {
+            System.out.println("msg");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    public void shutdown() {
+        this.running = false;
     }
 }
